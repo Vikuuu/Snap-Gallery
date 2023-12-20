e70e6dda1b51d05fcef2fa39f47127abfa7db52e
Aditya Vikram Singh <adivik672@gmail.com>
2023-12-20 22:02:39 +0530

    Settings updated

--- snapgallery/settings.py
+++ snapgallery/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Local Apps
     "accounts",
+    "photos",
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = "snapgallery.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
